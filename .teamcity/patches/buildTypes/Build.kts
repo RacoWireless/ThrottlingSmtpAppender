71ee902f9edd1676d97ee00922e5946ccbbb5cac
ckimball <ckimball@TeamCity>
2019-06-05 08:26:47 -0400

    TeamCity settings change: convert settings from version 875 to version 898

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,8 +1,10 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
-import jetbrains.buildServer.configs.kotlin.v2018_1.BuildStep
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.NuGetInstallerStep
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.VisualStudioStep
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.nuGetInstaller
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.nuGetPack
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.visualStudio
 import jetbrains.buildServer.configs.kotlin.v2018_1.ui.*
 
@@ -13,18 +15,17 @@ accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
     expectSteps {
-        step {
-            type = "jb.nuget.installer"
-            param("nuget.path", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
-            param("nuget.sources", """
+        nuGetInstaller {
+            toolPath = "%teamcity.tool.NuGet.CommandLine.DEFAULT%"
+            projects = "source/Log4Net.ThrottlingSmtpAppender.sln"
+            sources = """
                 "https://www.nuget.org/api/v2/"
                 "C:\NuGetPackages"
                 %teamcity.nuget.feed.auth.server%
-            """.trimIndent())
+            """.trimIndent()
             param("nuget.updatePackages.mode", "sln")
             param("nugetCustomPath", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
             param("nugetPathSelector", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
-            param("sln.path", "source/Log4Net.ThrottlingSmtpAppender.sln")
         }
         visualStudio {
             path = "source/Log4Net.ThrottlingSmtpAppender.sln"
@@ -35,27 +36,26 @@ changeBuildType(RelativeId("Build")) {
             configuration = "Release"
             param("octopus_octopack_package_version", "0.%sharedBuildNumber.id4%")
         }
-        step {
+        nuGetPack {
             name = "Package"
-            type = "jb.nuget.pack"
-            param("nuget.pack.as.artifact", "true")
-            param("nuget.pack.include.sources", "true")
-            param("nuget.pack.output.clean", "true")
-            param("nuget.pack.output.directory", "Build/ackages")
-            param("nuget.pack.specFile", "source/Log4Net.ThrottlingSmtpAppender.csproj")
-            param("nuget.pack.version", "0.0.1.%teamcity.build.id%")
-            param("nuget.path", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
+            toolPath = "%teamcity.tool.NuGet.CommandLine.DEFAULT%"
+            paths = "source/Log4Net.ThrottlingSmtpAppender.csproj"
+            version = "0.0.1.%teamcity.build.id%"
+            outputDir = "Build/ackages"
+            cleanOutputDir = true
+            publishPackages = true
+            includeSymbols = true
             param("nugetCustomPath", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
             param("nugetPathSelector", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
         }
     }
     steps {
-        update<BuildStep>(0) {
-            param("nuget.sources", """
+        update<NuGetInstallerStep>(0) {
+            sources = """
                 "https://www.nuget.org/api/v2/"
                 "C:\NuGetPackages"
                 %teamcity.nuget.feed.httpAuth._Root.default.v2%
-            """.trimIndent())
+            """.trimIndent()
         }
     }
 }
